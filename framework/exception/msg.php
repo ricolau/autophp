7c7bc6872cf21de91c80e14edadaab5c53292487
ricolau <ricolau@qq.com>
2016-05-16 17:17:41 +0800

    update exception_msg for bug fix

--- framework/exception/msg.php
+++ framework/exception/msg.php
@@ -11,25 +11,25 @@ class exception_msg extends Exception {
     public function __construct($message = "", $code = 0, \Exception $previous = null){
         parent::__construct($message, $code, $previous);
     }
-    public function getCode(){
-        return parent::getCode();
-    }
-    public function getMessage(){
-        return parent::getMessage();
-    }
-    public function getFile(){
-        return parent::getFile();
-    }
-    public function getLine(){
-        return parent::getLine();
-    }
-    public function getTrace(){
-        return parent::getTrace();
-    }
-    public function getTraceAsString(){
-        return parent::getTraceAsString();
-    }
-    public function getPrevious(){
-        return parent::getPrevious();
-    }
+//    public function getCode(){
+//        return parent::getCode();
+//    }
+//    public function getMessage(){
+//        return parent::getMessage();
+//    }
+//    public function getFile(){
+//        return parent::getFile();
+//    }
+//    public function getLine(){
+//        return parent::getLine();
+//    }
+//    public function getTrace(){
+//        return parent::getTrace();
+//    }
+//    public function getTraceAsString(){
+//        return parent::getTraceAsString();
+//    }
+//    public function getPrevious(){
+//        return parent::getPrevious();
+//    }
 }
