3cee138461ea4defb5de4ab1b90cbde4dd85c38d
ricolau <ricolau@qq.com>
2016-07-26 14:11:29 +0800

    demo update

--- demo2/config/dbmysql.php
+++ demo2/config/dbmysql.php
@@ -6,7 +6,9 @@
  * @desc default config file
  *
  */
-return array(
+return 
+
+$dev= array(
     'default' => array(
         'type' => 'mysqlpdo', //定义数据库类型，将直接调用 db_mysqlpdo 驱动
         'conf'=>array(
@@ -41,3 +43,85 @@ return array(
 
     )
 );
+
+$test = $dev= array(
+    'default' => array(
+        'type' => 'mysqlpdo', //定义数据库类型，将直接调用 db_mysqlpdo 驱动
+        'conf'=>array(
+            db_mysqlpdo::type_server_slave => array(
+                'host' => 'localhost',
+                'user' => 'database_username',
+                'pwd' => 'database_password',
+                'dbname' => 'database_name',
+                'charset' => 'utf8'
+            ),
+            db_mysqlpdo::type_server_master => array('host' => 'localhost', 'user' => 'root', 'pwd' => '', 'port' => 3306, 'dbname' => 'test', 'charset' => 'utf8')
+
+        ),
+
+    ),
+    'microspace' => array(
+        'type' => 'mysqlpdo',
+        'conf'=>array(
+            db_mysqlpdo::type_server_slave => array('host' => 'localhost', 'user' => 'root', 'pwd' => '', 'port' => 3306, 'dbname' => 'database_name2'),
+            db_mysqlpdo::type_server_master => array()
+        )
+
+    ),
+    // 微空间套餐方案库
+    'entStock' => array(
+        'type' => 'mysqlpdo',
+        'conf'=>array(
+            db_mysqlpdo::type_server_slave => array('host' => 'localhost', 'user' => 'database_username_slave', 'pwd' => '222222', 'dbname' => 'database_name22', 'charset' => 'utf8'),
+            db_mysqlpdo::type_server_master => array('host' => 'localhost', 'user' => 'database_username', 'pwd' => '111111', 'dbname' => 'database_name1', 'charset' => 'utf8')
+
+        )
+
+    )
+);
+
+$online = $dev= array(
+    'default' => array(
+        'type' => 'mysqlpdo', //定义数据库类型，将直接调用 db_mysqlpdo 驱动
+        'conf'=>array(
+            db_mysqlpdo::type_server_slave => array(
+                'host' => 'localhost',
+                'user' => 'database_username',
+                'pwd' => 'database_password',
+                'dbname' => 'database_name',
+                'charset' => 'utf8'
+            ),
+            db_mysqlpdo::type_server_master => array('host' => 'localhost', 'user' => 'root', 'pwd' => '', 'port' => 3306, 'dbname' => 'test', 'charset' => 'utf8')
+
+        ),
+
+    ),
+    'microspace' => array(
+        'type' => 'mysqlpdo',
+        'conf'=>array(
+            db_mysqlpdo::type_server_slave => array('host' => 'localhost', 'user' => 'root', 'pwd' => '', 'port' => 3306, 'dbname' => 'database_name2'),
+            db_mysqlpdo::type_server_master => array()
+        )
+
+    ),
+    // 微空间套餐方案库
+    'entStock' => array(
+        'type' => 'mysqlpdo',
+        'conf'=>array(
+            db_mysqlpdo::type_server_slave => array('host' => 'localhost', 'user' => 'database_username_slave', 'pwd' => '222222', 'dbname' => 'database_name22', 'charset' => 'utf8'),
+            db_mysqlpdo::type_server_master => array('host' => 'localhost', 'user' => 'database_username', 'pwd' => '111111', 'dbname' => 'database_name1', 'charset' => 'utf8')
+
+        )
+
+    )
+);
+
+
+
+if(auto::isDevMode()){
+    return $dev;
+}elseif(auto::isTestMode()){
+    return $test;
+}else{
+    return $online;
+}
