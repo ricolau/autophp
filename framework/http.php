7acd14c85459e44046dde5047ff48730d6f5fb16
ricolau <ricolau@qq.com>
2017-07-21 18:48:27 +0800

    update http class, add retry times when fail

--- framework/http.php
+++ framework/http.php
@@ -3,7 +3,7 @@
 /**
  * @description popular development for http request
  * @author by ricolau<ricolau@qq.com>
- * @version 2016-08-09
+ * @version 2017-07-21
  *
  */
 class http {
@@ -22,6 +22,7 @@ class http {
             useragent = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2041.4 Safari/537.36'
             proxy = null,
             timeout = 10,
+            retry_times=0, //retry times when fail
       );
      */
     public static function requestHigh($args) {
@@ -31,91 +32,103 @@ class http {
         if (!$args || !$args['url']) {
             return false;
         }
-        $_debugMicrotime = microtime(true);
         extract($args);
+        $request_times_max = ($retry_times ?:0)+1;
+        while($request_times_max>0){
+            $request_times_max--;
 
-        $method = $method ? : 'GET';
-        $timeout = $timeout ? : 3;
-
-        if (!function_exists('curl_init')){
-            exit('Need to open the curl extension');
-        }
-
-        $method = strtoupper($method);
-        $ci = curl_init();
-        $default_ua = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2041.4 Safari/537.36';
-        $useragent = $user_agent ? : $default_ua;
-        curl_setopt($ci, CURLOPT_USERAGENT, $useragent);
-        
-        curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 3);
-        curl_setopt($ci, CURLOPT_TIMEOUT, $timeout);
-        curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ci, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ci, CURLOPT_HEADER, ($return_header || $return_rich_info) ? true : false);
-
-        if ($proxy) {
-            curl_setopt($ci, CURLOPT_PROXY, $proxy);
-        }
-
-        if ($cookie){
-            curl_setopt($ci, CURLOPT_COOKIE, $cookie);
-        }
-
-        if ($referer){
-            curl_setopt($ci, CURLOPT_REFERER, $referer);
-        }
-
-        $headers = (array) $extheaders;
-        switch ($method) {
-            case 'POST':
-                curl_setopt($ci, CURLOPT_POST, TRUE);
-                if (!empty($params)) {
-                    if ($multi) {
-                        foreach ($multi as $key => $file) {
-                            $params[$key] = '@' . $file;
-                        }
-                        curl_setopt($ci, CURLOPT_POSTFIELDS, $params);
-                        $headers[] = 'Expect: ';
-                    } else {
-                        curl_setopt($ci, CURLOPT_POSTFIELDS, is_array($params) ? http_build_query($params) : $params);
-                    }
-                }
-                break;
-            case 'PUT':
-            case 'PATCH':
-                curl_setopt($ci, CURLOPT_POSTFIELDS, is_array($params) ? http_build_query($params) : $params);
-                curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method);
-                break;
-            case 'DELETE':
-            case 'GET':
-                $method == 'DELETE' && curl_setopt($ci, CURLOPT_CUSTOMREQUEST, 'DELETE');
-                if (!empty($params)) {
-                    $url = $url . (strpos($url, '?') ? '&' : '?')
-                            . (is_array($params) ? http_build_query($params) : $params);
-                }
-                break;
-        }
-        curl_setopt($ci, CURLINFO_HEADER_OUT, TRUE);
-        curl_setopt($ci, CURLOPT_URL, $url);
-        if ($headers) {
-            curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
-        }
-
-        $response = curl_exec($ci);
-        if ($return_rich_info) {
-            $info = curl_getinfo($ci);
-            $ret = array();
-            $ret['info'] = $info;
-            $ret['response']['header'] = substr($response, 0, $info['header_size']);
-            $ret['response']['content'] = substr($response, $info['header_size']);
+            $_debugMicrotime = microtime(true);
             
-            $response = $ret;
-        }
 
-        curl_close($ci);
-        ($timeCost = microtime(true) - $_debugMicrotime) && performance::add(__METHOD__, $timeCost, array('args'=>$args,'ret'=>performance::summarize($response,__METHOD__) )) ;
+            $method = $method ? : 'GET';
+            $timeout = $timeout ? : 2;
 
+            if (!function_exists('curl_init')){
+                exit('Need to open the curl extension');
+            }
+
+            $method = strtoupper($method);
+            $ci = curl_init();
+            $default_ua = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2041.4 Safari/537.36';
+            $useragent = $user_agent ? : $default_ua;
+            curl_setopt($ci, CURLOPT_USERAGENT, $useragent);
+
+            curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 3);
+            curl_setopt($ci, CURLOPT_TIMEOUT, $timeout);
+            curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ci, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ci, CURLOPT_HEADER, ($return_header || $return_rich_info) ? true : false);
+
+            if ($proxy) {
+                curl_setopt($ci, CURLOPT_PROXY, $proxy);
+            }
+
+            if ($cookie){
+                curl_setopt($ci, CURLOPT_COOKIE, $cookie);
+            }
+
+            if ($referer){
+                curl_setopt($ci, CURLOPT_REFERER, $referer);
+            }
+
+            $headers = (array) $extheaders;
+            switch ($method) {
+                case 'POST':
+                    curl_setopt($ci, CURLOPT_POST, TRUE);
+                    if (!empty($params)) {
+                        if ($multi) {
+                            foreach ($multi as $key => $file) {
+                                $params[$key] = '@' . $file;
+                            }
+                            curl_setopt($ci, CURLOPT_POSTFIELDS, $params);
+                            $headers[] = 'Expect: ';
+                        } else {
+                            curl_setopt($ci, CURLOPT_POSTFIELDS, is_array($params) ? http_build_query($params) : $params);
+                        }
+                    }
+                    break;
+                case 'PUT':
+                case 'PATCH':
+                    curl_setopt($ci, CURLOPT_POSTFIELDS, is_array($params) ? http_build_query($params) : $params);
+                    curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method);
+                    break;
+                case 'DELETE':
+                case 'GET':
+                    $method == 'DELETE' && curl_setopt($ci, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                    if (!empty($params)) {
+                        $url = $url . (strpos($url, '?') ? '&' : '?')
+                                . (is_array($params) ? http_build_query($params) : $params);
+                    }
+                    break;
+            }
+            curl_setopt($ci, CURLINFO_HEADER_OUT, TRUE);
+            curl_setopt($ci, CURLOPT_URL, $url);
+            if ($headers) {
+                curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
+            }
+
+            $success = true;
+            $response = curl_exec($ci);
+            if ($return_rich_info) {
+                $info = curl_getinfo($ci);
+                $ret = array();
+                $ret['info'] = $info;
+                $ret['response']['header'] = $info['header_size'] ? substr($response, 0, $info['header_size']): false;
+                $ret['response']['content'] = $info['header_size'] ? substr($response, $info['header_size']): false;
+
+                $success = ($info['http_code'] > 0) ? true : false;
+                $response = $ret;
+            }else{
+                $success = ($response!==false) ? true :false;
+            }
+
+            curl_close($ci);
+            ($timeCost = microtime(true) - $_debugMicrotime) && performance::add(__METHOD__, $timeCost, array('args'=>$args,'ret'=>performance::summarize($response,__METHOD__) )) ;
+            if($success){//break while retry
+                break;
+            }
+        }//end while
         return $response;
     }
 
