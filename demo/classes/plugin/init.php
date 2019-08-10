82e9367264e14adbc39303c958e6717ea8185b4f
rico <ricolau@qq.com>
2019-08-10 16:41:20 +0800

    Update init.php
    update

--- demo/classes/plugin/init.php
+++ demo/classes/plugin/init.php
@@ -8,7 +8,7 @@ class plugin_init extends plugin_abstract{
     
     
     public function call($tag, plugin_context &$ptx){
-        $ptx->breakOut = 'hahahaha';
+        //$ptx->breakOut = 'hahahaha';
         echo "\r\n <br /> hello, im plugin wbinit before your action run! \r\n <br /> ";
     }
     
