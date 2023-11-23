bc7ff32a7ef274f9e2aac8742c7d7675370c3efc
liuruizhou.lrz <liuruizhou.lrz@alibaba-inc.com>
2023-11-23 14:11:57 +0800

    update http request

--- framework/http.php
+++ framework/http.php
@@ -3,7 +3,7 @@
 /**
  * @description popular development for http request
  * @author by ricolau<ricolau@qq.com>
- * @version 2017-07-21
+ * @version 2023-11-23
  *
  */
 class http {
@@ -21,13 +21,16 @@ class http {
             return_rich_info = null,
             user_agent = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2041.4 Safari/537.36'
             proxy = null,
+            proxyport = null
+            proxytype  = CURLPROXY_SOCKS5 /  CURLPROXY_HTTP 
+            connect_timeout = 3,
             timeout = 10,
             retry_times=0, //retry times when fail
       );
      */
     public static function requestHigh($args) {
         if (!function_exists('curl_init')){
-            throw new exception_base('curl module not exist~!');
+            throw new exception_base('curl module not exist~!', -1);
         }
         if (!$args || !$args['url']) {
             return false;
@@ -39,9 +42,8 @@ class http {
 
             $_debugMicrotime = microtime(true);
             
-
-            $method = $method ? : 'GET';
-            $timeout = $timeout ? : 2;
+            $method = isset($method) ? $method : 'GET';
+            $timeout = isset($timeout) ?$timeout  : 2;
 
             if (!function_exists('curl_init')){
                 exit('Need to open the curl extension');
@@ -50,28 +52,35 @@ class http {
             $method = strtoupper($method);
             $ci = curl_init();
             $default_ua = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2041.4 Safari/537.36';
-            $user_agent = $user_agent ? : $default_ua;
-            curl_setopt($ci, CURLOPT_USERAGENT, $user_agent);
+            $useragent =  isset($user_agent) ?$user_agent : $default_ua;
+            curl_setopt($ci, CURLOPT_USERAGENT, $useragent);
+            $return_header = isset($return_header) ? $return_header : false;
+            $return_rich_info = isset($return_rich_info) ? $return_rich_info : false;
 
-            curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 3);
+            curl_setopt($ci, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, (isset($connect_timeout) && $connect_timeout>0)? $connect_timeout : 3 );
             curl_setopt($ci, CURLOPT_TIMEOUT, $timeout);
             curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ci, CURLOPT_SSL_VERIFYHOST, false);
             curl_setopt($ci, CURLOPT_HEADER, ($return_header || $return_rich_info) ? true : false);
 
-            if ($proxy) {
+            $proxy = isset($proxy) ? $proxy : '';
+            if ($proxy && $proxyport) {
                 curl_setopt($ci, CURLOPT_PROXY, $proxy);
+                curl_setopt($ci, CURLOPT_PROXYPORT, $proxyport);
+                curl_setopt($ci, CURLOPT_PROXYTYPE, $proxytype == CURLPROXY_SOCKS5 ? CURLPROXY_SOCKS5:  CURLPROXY_HTTP );  // CURLPROXY_SOCKS5
             }
-
+            $cookie = isset($cookie) ? $cookie : '';
             if ($cookie){
                 curl_setopt($ci, CURLOPT_COOKIE, $cookie);
             }
-
+            $referer = isset($referer) ? $referer : '';
             if ($referer){
                 curl_setopt($ci, CURLOPT_REFERER, $referer);
             }
 
+            $extheaders = isset($extheaders) ? $extheaders : [];
             $headers = (array) $extheaders;
             switch ($method) {
                 case 'POST':
@@ -112,11 +121,16 @@ class http {
             $response = curl_exec($ci);
             if ($return_rich_info) {
                 $info = curl_getinfo($ci);
+                $error = curl_error($ci);
+                if($error){
+                    $info['error'] = $error;
+                }
+
                 $ret = array();
                 $ret['info'] = $info;
                 $ret['response']['header'] = $info['header_size'] ? substr($response, 0, $info['header_size']): false;
                 $ret['response']['content'] = $info['header_size'] ? substr($response, $info['header_size']): false;
-
+                
                 $success = ($info['http_code'] > 0) ? true : false;
                 $response = $ret;
             }else{
@@ -138,18 +152,18 @@ class http {
      * 
      * @param string $url
      * @param string $params
-     * @param type $method
-     * @param type $timeout
-     * @param type $cookie
-     * @param type $referer
-     * @param type $extheaders
-     * @param type $multi
-     * @param type $proxy
-     * @return type
+     * @param string $method
+     * @param int $timeout
+     * @param string $cookie
+     * @param string $referer
+     * @param array $extheaders
+     * @param array $multi
+     * @param string $proxy
+     * @return string
      */
-    public static function request($url, $params = array(), $method = 'GET', $timeout = 3, $cookie = '', $referer = null, $extheaders = array(), $multi = false, $proxy = null) {
+    public static function request($url, $params = array(), $method = 'GET', $timeout = 3, $cookie = '', $referer = null, $extheaders = array(), $multi = false) {
         if (!function_exists('curl_init')){
-            throw new exception_base('curl module not exist~!');
+            throw new exception_base('curl module not exist~!',-1);
         }
         $_debugMicrotime = microtime(true);
 
